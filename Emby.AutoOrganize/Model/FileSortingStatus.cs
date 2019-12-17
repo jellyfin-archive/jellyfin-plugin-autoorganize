e83233191345a19567d2e460afac5fd4f7ddfb18
Mark Monteiro <mark.monteiro23@gmail.com>
2019-12-17 21:12:02 +0100

    Document enumerations

--- Emby.AutoOrganize/Model/FileSortingStatus.cs
+++ Emby.AutoOrganize/Model/FileSortingStatus.cs
@@ -1,9 +1,23 @@
 namespace Emby.AutoOrganize.Model
 {
+    /// <summary>
+    /// The result status of sorting a single item.
+    /// </summary>
     public enum FileSortingStatus
     {
+        /// <summary>
+        /// File sorting completed successfully.
+        /// </summary>
         Success,
+
+        /// <summary>
+        /// File sorting failed due to an error.
+        /// </summary>
         Failure,
+
+        /// <summary>
+        /// File sorting was skipped on this item because it already exists in the target library.
+        /// </summary>
         SkippedExisting
     }
 }
